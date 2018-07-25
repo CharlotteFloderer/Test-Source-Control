43ef4d71afe0f9c8b7357ef4bdb40c3ce51b9a23
Floderer Charlotte <charlotte@macbook-pro-de-floderer.local>
2018-07-25 17:40:23 +0200

    Ajout d'une class test

---
+++ Test Source Control/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Test Source Control
+//
+//  Created by Floderer Charlotte on 25/07/2018.
+//  Copyright © 2018 Floderer Charlotte. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut () {
+        print("Salut salut")
+    }
+}
