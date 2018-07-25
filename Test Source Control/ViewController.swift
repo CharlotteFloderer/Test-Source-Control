82f6b9b500ae6a3aff19f362b56f4e7026703e05
Floderer Charlotte <charlotte@macbook-pro-de-floderer.local>
2018-07-25 17:22:03 +0200

    Initial Commit

---
+++ Test Source Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Source Control
+//
+//  Created by Floderer Charlotte on 25/07/2018.
+//  Copyright © 2018 Floderer Charlotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
